db5a0635680a42207afe9a1a705828f7c7e91e1c
Rakesh Singh <sainirakesh3221@gmail.com>
2025-02-20 22:30:01 +0530

    change database

--- GuriMobile-api/config/db.js
+++ GuriMobile-api/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function ConnectedDB(){
 
-    mongoose.connect("mongodb://localhost:27017/guri_garage")
+    mongoose.connect("mongodb+srv://sainirakesh3221:Saini3221@gurimobilegarage.z39ii.mongodb.net/")
     .then(()=>
         console.log("connect to data base")
     )
